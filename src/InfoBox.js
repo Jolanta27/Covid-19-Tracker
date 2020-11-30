d7586b0f00fe24e1f94778220af743779dbb7954
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2020-12-01 00:21:01 +0100

    last small changes

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,22 +2,29 @@ import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./infoBox.css";
 
-function InfoBox({title, cases, isRed, active, total, ...props}) {
-    return (
-        <Card 
-        onClick={props.onClick}
-        className={`infoBox ${active && 'infoBox--selected'} ${isRed && "infoBox––red"}`}>
-            <CardContent>
-            <Typography className="infoBox__title" color="textSecondary">
-            {title}
-            </Typography>
-            <h2 className="infoBox__cases">{cases}</h2>
-            <Typography className="infoBox__total"  color="textSecondary">
-                {total} Total
-            </Typography>
-            </CardContent>
-        </Card>
-    )
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
+
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
